9ea1770684dbe1e7815ea65b4f2f9d59436fe91e
AdobakBU <adobak@bu.edu>
2024-11-03 13:04:04 -0500

    added chatopenAI library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
